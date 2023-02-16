12eb2bf229fba81dbfb8ee8212ee70573fc2ef03
filipwramdemark <filip.wramdemark@hotmail.com>
2023-02-16 16:30:09 +0100

    nn, train and test split

--- algoritm/NN.py
+++ algoritm/NN.py
@@ -1,7 +1,17 @@
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
 
-df = pd.read_excel('algoritm/Valladata.xlsx')
+# read the data
+data = pd.read_excel('algoritm/Valladata.xlsx')
 
-print(df)
+# split into training and test set
+train, test = train_test_split(data, test_size = 0.2, shuffle = True)
+
+train_labels = train['Valla (Label)']
+train_features = train.drop(columns = 'Valla (Label)')
+test_labels = test['Valla (Label)']
+test_features = test.drop(columns = 'Valla (Label)')
+
+
