4b29b481b1b5c570a7f0e1588203b26adc79b493
nora.a.larsson@hotmail.com <nora.a.larsson@hotmail.com>
2023-02-20 09:58:56 +0100

    Update KNN.py

--- algoritm/KNN.py
+++ algoritm/KNN.py
@@ -1,1 +1,26 @@
-print('valla är svårt')
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+import math
+
+
+
+#data = pd.read_excel(r'algoritm/Valladata.xlsx')
+data = pd.read_csv(r'algoritm/Valladata_prep.csv')
+print(data)
+
+#snowtemp = list(data['Snötemperatur:'])
+#print(snowtemp)
+#snowtype = list(data['Snötyp:'])
+#print(snowtype)
+
+#A = data.iloc[0:,0:3]
+#print(A)
+
+#lables = data.labels_
+
+##residue_name = list(data.index)
+#print(residue_name)
+
+#print('hello')
