49f811849c1af552a10cc89d3fa3700f125f8e9b
filipwramdemark <filip.wramdemark@hotmail.com>
2023-02-23 14:33:03 +0100

    updated website

--- website.py
+++ website.py
@@ -1,41 +1,50 @@
 from flask import Flask, render_template, request, redirect, url_for
-from algoritm.NN.NN_predict import predict
+import algoritm.NN.NN_predict as NN
 import pandas as pd
 
-snow = ''
-
 snow_to_label_df = pd.read_csv('algoritm/snow_to_label.csv', names=['Snow', 'Label'])
-snow_to_label = snow_to_label_df.to_dict(orient='list')
+
+snow_types_df = snow_to_label_df['Snow']
+
+
+snow_to_label = snow_to_label_df.to_dict(orient='list') 
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        if request.form.get('action1') == 'Konstsnö':
-            global snow
-            snow = 'Konstsnö'
-        elif  request.form.get('action2') == 'Nysnö':
-            snow = 'Nysnö'
-        return redirect(url_for('temp'))
-        
-    elif request.method == 'GET':
-        return render_template('index.html')
+    return render_template('index.html', snow_types={0 : 'Konstsnö', 1 : 'Nysnö', 2 : 'Blandad snö', 3 : 'Skitig snö'})
+
+
+# @app.route('/temp', methods=['GET'])
+# def temp():
+#     return render_template('temp.html')
+
+
+# @app.route('/temp', methods=['POST'])
+# def temp_post():
+#     print("Temp:", request.form)
+#     # T = float(request.form['text'])
+#     # snow_ind = snow_to_label['Snow'].index(snow)
+#     # snow_label = snow_to_label['Snow'][snow_ind]
+#     # wax_label = predict([snow_label, T])
+#     return render_template('label.html', label="hej")
+
+@app.route('/snowtypes/<snow_type>', methods=["GET", "POST"])
+def snow_types(snow_type : int):
+    if request.method == "GET":
+        return render_template('temp.html', snow_type=snow_type)
+    elif request.method == "POST":
+        T = float(request.form['temp'])
+
     
-    return render_template('index.html')
+        wax = NN.predict([snow_type, T])
 
-@app.route('/temp')
-def temp():
-    return render_template('temp.html')
+        # return render_template('label.html', label=wax)
+        return wax
+    else:
+        return 404
 
-
-@app.route('/temp', methods=['POST'])
-def temp_post():
-    T = float(request.form['text'])
-    snow_ind = snow_to_label['Snow'].index(snow)
-    snow_label = snow_to_label['Snow'][snow_ind]
-    wax_label = predict([snow_label, T])
-    return wax_label[0]
 
 if __name__ == "__main__":
     app.run()
