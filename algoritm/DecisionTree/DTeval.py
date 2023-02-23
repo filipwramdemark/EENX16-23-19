f9c09081af5acf25143d62af0975f9ca39cd4ac4
Kuff99 <k.waidringer@hotmail.com>
2023-02-23 13:46:18 +0100

    Update DTeval.py

--- algoritm/DecisionTree/DTeval.py
+++ algoritm/DecisionTree/DTeval.py
@@ -1,10 +1,10 @@
-import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import pickle
-from joblib import load, dump
 import graphics as gp
 from Decisiontree import Classification_eval
+
+
 tree = pickle.load(open('algoritm/DecisionTree/Decision_Tree.pickle', "rb"))
 data = pd.read_csv('algoritm/Valladata_prep.csv')
 data = data.dropna(axis='columns')   
