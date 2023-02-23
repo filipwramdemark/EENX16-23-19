5483b30920a0f6412af0bc498ca63718ee5a5048
filipwramdemark <filip.wramdemark@hotmail.com>
2023-02-23 10:20:22 +0100

    implemented buttons

--- website/website.py
+++ website/website.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
+
+def cal(x):
+    return x + 1
 
 app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'Konstsnö':
+            x = cal(3)
+            return ("Swix V40" + str(x))
+        elif  request.form.get('action2') == 'Nysnö':
+            x = cal(5)
+            return ("Swix KX75" + str(x))
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('index.html')
+    
+    return render_template("index.html")
+
+# @app.route("/test/")
+# def test():
+#     return "Hello! this is the main page <h1>HELLO</h1>"
 
 if __name__ == "__main__":
     app.run()
 
-# Defining the home page of our site
-@app.route("/")  # this sets the route to this page
-def home():
-	return "Hello! this is the main page <h1>HELLO</h1>"
