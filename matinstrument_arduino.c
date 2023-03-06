2c1f58744ea71f0bd1b97e6ea263f2441fc52fa8
xQubeZ <45330808+xQubeZ@users.noreply.github.com>
2023-03-06 20:31:26 +0100

    Update matinstrument_arduino.c

--- matinstrument_arduino.c
+++ matinstrument_arduino.c
@@ -1,16 +1,21 @@
 #include <ArduinoBLE.h>
+#include <Arduino_LSM6DSOX.h>
 
-BLEService SendData("64981768-2f1b-400e-8e75-e5a86c5434b8");
-BLEFloatCharacteristic SendCharacteristic("64981768-2f1b-400e-8e75-e5a86c5434b8", BLERead | BLENotify);
+BLEService SendData("19B10000-E8F2-537E-4F6C-D104768A1214");
+BLEIntCharacteristic SendCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLENotify);
 
 void setup() {
   Serial.begin(9600);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+  }
 
   if(!BLE.begin()){
     Serial.println("BLE start failed");
   }
 
-  BLE.setLocalName("Data sender");
+  BLE.setLocalName("TempData");
   BLE.setAdvertisedService(SendData);
 
   SendData.addCharacteristic(SendCharacteristic);
@@ -33,10 +38,17 @@ void loop() {
 
   while(central.connected()){
     //Skriv kod för sensor data
-    int temp = 1;
+    //int temp = 43;
+    int temp = 0;
 
-    //Skicka datan
-    SendCharacteristic.writeValue(temp);
+    if(IMU.temperatureAvailable()){
+      IMU.readTemperature(temp);
+      Serial.println(temp);
+      //Skicka datan
+      SendCharacteristic.writeValue(temp);
+    }
+
+    
   }
 
   Serial.println("Central disconnected");
