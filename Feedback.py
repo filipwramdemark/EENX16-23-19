3f13842ebc0f53602ae65cb7f538b493f3e5a85d
Kuff99 <k.waidringer@hotmail.com>
2023-03-24 13:38:40 +0100

    cool

--- Feedback.py
+++ Feedback.py
@@ -3,7 +3,7 @@ import pandas as pd
 data = [0,1, 2]
 
 def savefeedback(da):
-    feedback =  {'Snötyp:': [da[0]], 'Snötemperatur:': [da[1]]}
+    feedback =  {'Snötyp:': [da[0]], 'Snötemperatur:': [da[1]], 'Valla (Label)': [da[2]]}
     testdata = pd.DataFrame.from_dict(feedback)
     datatoexcel = pd.ExcelWriter('algoritm/Feedback.xlsx')
     testdata.to_excel(datatoexcel)
@@ -12,7 +12,16 @@ def savefeedback(da):
     return
 
 savefeedback(data)
+old = pd.read_csv('algoritm/Valladata_prep.csv')
 
+old = old.head(5)
+print('gamla')
+print(old)
 data = pd.read_excel('algoritm/Feedback.xlsx')
+print('nya')
 print(data)
+frames = [old,data]
+test =  pd.concat(frames)
 
+print('Final')
+print(test)
