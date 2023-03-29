a40357e2c75b16ed4cfa4983c4a651b98399b3e7
Kuff99 <k.waidringer@hotmail.com>
2023-03-29 15:34:39 +0200

    Push

--- algoritm/DecisionTree/Tree_Predict.py
+++ algoritm/DecisionTree/Tree_Predict.py
@@ -6,13 +6,13 @@ import pickle
 
 # data =  {'Snötyp:': [0], 'Snötemperatur:': [0]}
 # testdata = pd.DataFrame.from_dict(data)
-
+# 
 # test = [0,1]
 
 def Treepredict(lista):
     data =  {'Snötyp:': [lista[0]], 'Snötemperatur:': [lista[1]]}
     testdata = pd.DataFrame.from_dict(data)
-    model = pickle.load(open('algoritm/DecisionTree/Decision_Tree.pickle', "rb"))
+    model = pickle.load(open('algotim/DecisionTree/Decision_Tree.pickle', 'rb'))
     label_to_wax_df = pd.read_csv('algoritm/label_to_wax.csv', names=['Label', 'Wax'])    
     guessedlabel = model.predict(testdata.iloc[0])
     guess =(label_to_wax_df.at[guessedlabel, 'Wax'])
