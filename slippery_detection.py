801dfe777105fa2216e925d72efb332e6e30cc91
filipwramdemark <filip.wramdemark@hotmail.com>
2023-04-05 10:37:28 +0200

    Update slippery_detection.py

--- slippery_detection.py
+++ slippery_detection.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-acc = pd.read_csv('acc_test2.csv', header=None).values.tolist()
+acc = pd.read_csv('acc_test1.csv', header=None).values.tolist()
 
 x_acc = []
 y_acc = []
@@ -20,16 +20,18 @@ def slip_count():
     i = 0
     high_ind = []
     while i < len(x_acc):
-        if x_acc[i] > 0.6 * max_acc:
+        if x_acc[i] > 0.75 * max_acc:
             high_ind.append(i)
-            while (i < len(x_acc) - 1) and (x_acc[i+1] >  0.6 * max_acc):
+            while (i < len(x_acc) - 1) and (x_acc[i+1] >  0.75 * max_acc):
                 i += 1
         i += 1
-    f = (high_ind[-1] - high_ind[0]) / len(high_ind)
+    threshold = 40
 
     slip_counter = 0
     for i in range(len(high_ind)-1):
-        if (high_ind[i+1] - high_ind[i] < f / 2):
+        if (high_ind[i+1] - high_ind[i] < threshold):
             slip_counter += 1
 
-    return slip_counter
+    return (slip_counter / len(high_ind))
+
+print(slip_count())
