b98263b57870797f64467aa177958c8605ff9fe7
Kuff99 <k.waidringer@hotmail.com>
2023-04-24 08:09:43 +0200

    Träd klart#45

--- algoritm/DecisionTree/Tree_Predict.py
+++ algoritm/DecisionTree/Tree_Predict.py
@@ -10,7 +10,7 @@ from algoritm.DecisionTree.Decisiontree import *
 # test = [0,1]
 
 def Treepredict(lista):
-    data =  {'Snötyp:': [lista[0]], 'Snötemperatur:': [lista[1]]}
+    data =  {'Snötyp:': [lista[0]], 'Snötemperatur:': [lista[1]], 'Fuktighet snö': [lista[2]], 'Temperatur luft': [lista[3]], 'Luftfuktighet': [lista[4]]}
     testdata = pd.DataFrame.from_dict(data)
     model = pickle.load(open('algoritm/DecisionTree/Decision_Tree.sav', 'rb'))
     label_to_wax_df = pd.read_csv('algoritm/label_to_wax.csv', names=['Label', 'Wax'])    
